c9540824810b742fccf2deb314a7f08db694c5b6
PangZHi <66349953@qq.com>
2022-04-22 14:41:51 +0800

    session的protobuff创建

---
+++ PostRet/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/micro/go-micro"
+	"github.com/micro/go-micro/util/log"
+	"sss/PostRet/handler"
+	"sss/PostRet/subscriber"
+
+	PostRET "sss/PostRet/proto/PostRet"
+)
+
+func main() {
+	// New Service
+	service := micro.NewService(
+		micro.Name("go.micro.srv.PostRet"),
+		micro.Version("latest"),
+	)
+
+	// Initialise service
+	service.Init()
+
+	// Register Handler
+	PostRET.RegisterPostRetHandler(service.Server(), new(handler.PostRet))
+
+	// Register Struct as Subscriber
+	micro.RegisterSubscriber("go.micro.srv.PostRet", service.Server(), new(subscriber.PostRet))
+
+	// Register Function as Subscriber
+	micro.RegisterSubscriber("go.micro.srv.PostRet", service.Server(), subscriber.Handler)
+
+	// Run service
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
