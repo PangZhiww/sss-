ae2cea4271f11025f6042d7d1bed86c40be91ef3
PangZHi <66349953@qq.com>
2022-04-25 14:57:16 +0800

    DeleteSession创建

---
+++ DeleteSession/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/micro/go-micro"
+	"github.com/micro/go-micro/util/log"
+	"sss/DeleteSession/handler"
+	"sss/DeleteSession/subscriber"
+
+	DeleteSession "sss/DeleteSession/proto/DeleteSession"
+)
+
+func main() {
+	// New Service
+	service := micro.NewService(
+		micro.Name("go.micro.srv.DeleteSession"),
+		micro.Version("latest"),
+	)
+
+	// Initialise service
+	service.Init()
+
+	// Register Handler
+	DeleteSession.RegisterDeleteSessionHandler(service.Server(), new(handler.DeleteSession))
+
+	// Register Struct as Subscriber
+	micro.RegisterSubscriber("go.micro.srv.DeleteSession", service.Server(), new(subscriber.DeleteSession))
+
+	// Register Function as Subscriber
+	micro.RegisterSubscriber("go.micro.srv.DeleteSession", service.Server(), subscriber.Handler)
+
+	// Run service
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
